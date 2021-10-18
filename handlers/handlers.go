3814545dde0abf7c9d489305c4f5c4b9655e8c12
Nicolas Cieri <nicolas.cieri@globallogic.com>
2021-10-18 11:19:45 -0300

    [#2] Implement Chi and Hello World with env variable

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,37 +3,39 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-type Movie struct {
-	Title       string `json:"title"`
-	ReleaseDate int    `json:"ReleaseDate"`
-	Platform    string `json:"Platform"`
-}
-
-var movies = []Movie{
-	{Title: "Back to the Future", ReleaseDate: 1985, Platform: "Netflix"},
-	{Title: "Joker", ReleaseDate: 2019, Platform: "Prime Video"},
-	{Title: "Click", ReleaseDate: 2006, Platform: "HBO Max"},
-}
-
-func allMovies(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: All Movies Endpoint")
-	json.NewEncoder(w).Encode(movies)
+type Response struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Data    string `json:"data"`
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Homepage")
+	var response Response = Response{
+		Status:  http.StatusOK,
+		Message: "Hola Mundo",
+	}
+
+	json.NewEncoder(w).Encode(response)
 }
 
 func HandleRequests() {
+	var port string = os.Getenv("HTTP_PORT")
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", homePage)
-	r.Get("/movies", allMovies)
-	http.ListenAndServe(":8001", r)
+	log.Println("Servirn the app in the port:", port)
+	err := http.ListenAndServe(":"+port, r)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
