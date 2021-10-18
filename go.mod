6d70f5687532b376f108a6206abc5e7847c015a0
ncieriGL <92588110+ncieriGL@users.noreply.github.com>
2021-10-18 11:29:25 -0300

    Feature/#2 hello world chi (#17)
    * probando cambio den git
    
    * [#2] Implement Chi and Hello World with env variable

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module training/tutorialGL
 
 go 1.17
+
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-chi/chi/v5 v5.0.4
+)
--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"fmt"
-	
+	"training/tutorialGL/handlers"
 )
 
 func main() {
-	fmt.Println("Hola mundo!")
+	handlers.HandleRequests()
 }
